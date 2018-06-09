0bf518aa846541e324b057ee72a61dcf6fb07637
MarcinChorzepa <mchorzepa1@tlen.pl>
2018-06-09 18:38:07 +0200

    initial commit

---
+++ src/main/java/com/newbookstore/newbookstore/NewbookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.newbookstore.newbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbookstoreApplication.class, args);
+    }
+}
---
+++ src/test/java/com/newbookstore/newbookstore/NewbookstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newbookstore.newbookstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewbookstoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
